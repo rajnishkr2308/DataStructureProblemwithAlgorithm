83bd6a18cd06d2e867c97946b5711fe6a56c0017
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-07 23:41:17 +0530

    Added:UnorderdNode

--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -1,4 +1,17 @@
 package com.bridgelabz;
-public class Node{
+    public class Node <T> {
+        /*
+           Initializing Data and Node using Generics
+         */
+        T data;
+        Node<T> next;
 
-}
+        public Node() {
+        }
+        public Node(T data) {
+        /*
+       Parameter Constructor
+     */
+            this.data = data;
+        }
+    }
